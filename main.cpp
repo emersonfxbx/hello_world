ce08b71e3858950520351618f72692f40c253f29
Emerson de Freitas Barcelos <emersonfxbx@msn.com>
2014-03-17 17:51:25 -0300

    Teste

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// main.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int
+main(int argc, char const *argv[])
+{
+	std::cout << "Hello World...(2)\n";
+	_getch();
+	return 0;
+}
+
